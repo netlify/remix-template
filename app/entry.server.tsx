787e25b57aef42d28470ab3af64fca45e3acbcde
Jacob Ebey <jacob.ebey@live.com>
2022-04-20 11:40:38 -0700

    updated to stream

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,20 +1,30 @@
 import type { EntryContext } from "@remix-run/server-runtime";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server";
+import isbot from "isbot";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  let body = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+    {
+      onError() {
+        responseStatusCode = 500;
+      },
+    }
   );
+
+  if (isbot(request.headers.get("user-agent"))) {
+    await body.allReady;
+  }
 
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(body, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
