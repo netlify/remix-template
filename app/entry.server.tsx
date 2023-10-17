1958dfc03a374801e80f9fd56283d172a2b662b7
ascorbic <ascorbic@gmail.com>
2023-10-17 21:15:47 +0100

    fix: use streaming entrypoint

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,74 @@
-import type { EntryContext } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { PassThrough } from 'node:stream'
+
+import type { AppLoadContext, EntryContext } from '@remix-run/node'
+import { createReadableStreamFromReadable } from './stream'
+import { RemixServer } from '@remix-run/react'
+import isbot from 'isbot'
+import { renderToPipeableStream } from 'react-dom/server'
+
+const ABORT_DELAY = 5_000
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
+  loadContext: AppLoadContext,
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  const bot = isbot(request.headers.get('user-agent'))
+  return new Promise((resolve, reject) => {
+    let shellRendered = false
+    const body = new PassThrough()
+    const stream = createReadableStreamFromReadable(body)
 
-  responseHeaders.set("Content-Type", "text/html");
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
+      {
+        onShellReady() {
+          if (!bot) {
+            shellRendered = true
+            responseHeaders.set('Content-Type', 'text/html')
+            resolve(
+              new Response(stream, {
+                headers: responseHeaders,
+                status: responseStatusCode,
+              }),
+            )
+            pipe(body)
+          }
+        },
+        onShellError(error: unknown) {
+          reject(error)
+        },
+        onAllReady() {
+          clearTimeout(timer)
+          if (bot) {
+            shellRendered = true
+            responseHeaders.set('Content-Type', 'text/html')
+            resolve(
+              new Response(stream, {
+                headers: responseHeaders,
+                status: responseStatusCode,
+              }),
+            )
+            pipe(body)
+          }
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error)
+          }
+        },
+      },
+    )
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    headers: responseHeaders,
-    status: responseStatusCode,
-  });
+    const timer = setTimeout(() => {
+      console.log('aborting')
+      abort()
+    }, ABORT_DELAY)
+  })
 }
