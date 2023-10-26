91a03ff5d19c10c343a4bdbe76d8cc42284d50be
ascorbic <ascorbic@gmail.com>
2023-10-26 19:00:35 +0100

    chore: fix server entry

--- server.ts
+++ server.ts
@@ -1,7 +1,16 @@
-import { createRequestHandler } from "@netlify/remix-adapter";
-import * as build from "@remix-run/dev/server-build";
+import * as build from '@remix-run/dev/server-build'
+import { createRequestHandler } from '@netlify/remix-adapter'
+import type { Context, Config } from '@netlify/functions'
 
-export const handler = createRequestHandler({
+const handle = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
-});
+})
+
+export default function handler(request: Request, context: Context) {
+  return handle(request, context)
+}
+
+export const config: Config = {
+  path: '/*',
+}
