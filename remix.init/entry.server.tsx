930e549c60a45610ced78972ee4573d826d43c79
Matt Kane <matt.kane@netlify.com>
2023-10-26 17:48:05 +0100

    chore: fix entrypoints

--- remix.init/entry.server.tsx
+++ remix.init/entry.server.tsx
@@ -8,7 +8,7 @@ export default async function handleRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  loadContext: AppLoadContext,
+  loadContext: AppLoadContext
 ) {
   const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
@@ -19,7 +19,7 @@ export default async function handleRequest(
         console.error(error);
         responseStatusCode = 500;
       },
-    },
+    }
   );
 
   if (isbot(request.headers.get("user-agent"))) {
