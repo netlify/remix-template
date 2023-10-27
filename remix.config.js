c2fc97d1f16fc749dd50b2b9c35ca348c4bdd8dc
Matt Kane <matt.kane@netlify.com>
2023-10-27 16:31:41 +0100

    fix: use default dev server for functions

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@ import { config } from "@netlify/remix-adapter";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...config,
+  ...(process.env.NODE_ENV === "production" ? config : undefined),
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
